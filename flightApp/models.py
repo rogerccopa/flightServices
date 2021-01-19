b64e740624f62954ce4a0c2271d45e4ea34af730
Roger Ccopa <rogerccopa@gmail.com>
2021-01-18 22:24:31 -0500

    Added data validators

--- flightApp/models.py
+++ flightApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Flight(models.Model):
     flightNumber = models.CharField(max_length=10)
     operatingAirlines = models.CharField(max_length=20)
-    departureCity = models.CharField(max_length=20)
+    departureCity = models.CharField(max_length=20, blank=True, null=True)
     arrivalCity = models.CharField(max_length=20)
     dateOfDeparture = models.DateField()
     estimatedTimeOfDeparture = models.TimeField()
--- flightApp/serializers.py
+++ flightApp/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
 from flightApp.models import Flight, Passenger, Reservation
+import re
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+    
+    # A custom validator should be of pattern: validate_<fieldName>
+    # all custom validators are invoked when called: <mySerializer>.is_valid()
+    def validate_flightNumber(self, flightNumber):
+        if (re.match("^[a-zA-Z0-9]*$", flightNumber) == None):
+            raise serializers.ValidationError("Invalid flight number. Make sure it is alphanumeric")
+        return flightNumber
+
+    # this method 'validate' is general for all fields (not just one in particular)
+    def validate(self, data):
+        # do validations here
+        print(data['flightNumber'])
+        return data
+
 
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
