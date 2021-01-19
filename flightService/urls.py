aa72707bc203b5cc9b4faf5a563e06cd7303413e
Roger Ccopa <rogerccopa@gmail.com>
2021-01-18 20:59:46 -0500

    find_flights & save_reservation implemented

---
+++ flightService/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from flightApp import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('flights', views.FlightViewSet)
+router.register('passengers', views.PassengerViewSet)
+router.register('reservations', views.ReservationViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('flightServices/', include(router.urls)),
+    path('flightServices/findFlights', views.find_flights),
+    path('flightServices/saveReservation', views.save_reservation)
+]
